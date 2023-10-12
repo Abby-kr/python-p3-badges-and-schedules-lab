f586ad6c8ae3c37f6023061dd8fa494d58866179
Abby-kr <abby.a.otieno@gmail.com>
2023-10-12 16:12:16 +0300

    Completed Lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,25 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = list()
+    for name in names:
+        badges.append(f"Hello, my name is {name}.")
+    return badges
 
 def assign_rooms(names):
-    return None
+    speaker_rooms = list()
+    index = 1
+    for name in names:
+        speaker_rooms.append(f"Hello, {name}! You'll be assigned to room {index}!")
+        index += 1
+    return speaker_rooms
 
 def printer(names):
-    return None
+    for name in names:
+        print(f"Hello, my name is {name}.",end="\n")
+    index = 1
+    for name in names:
+        print(f"Hello, {name}! You'll be assigned to room {index}!",end="\n")
+        index += 1
+    
